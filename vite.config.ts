d2ef2009dd1d2cd6466c40e132fb8436e6207918
nao_fan7 <naokot9535@gmail.com>
2025-12-28 09:02:10 +0900

    公開準備

--- vite.config.ts
+++ vite.config.ts
@@ -16,13 +16,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'icon-192.png',
+            src: 'cat.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
             type: 'image/png'
           }
         ]
